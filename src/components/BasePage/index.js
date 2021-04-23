9d0b28086a47c25ef2322bb54f3880f3f6b7ba5a
Vinicius Viana <vncs1995@gmail.com>
2021-04-23 00:18:29 -0300

    Changing BasePage return, deleting unnecessary style

--- src/components/BasePage/index.js
+++ src/components/BasePage/index.js
@@ -5,24 +5,22 @@ import PropTypes from 'prop-types';
 import COLORS from '../../constants/colors';
 import styles from './styles';
 
-const BasePage = ({children, style}) => {
-  return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <LinearGradient
-        start={{x: 1, y: 0}}
-        end={{x: 0, y: 1}}
-        colors={[COLORS.pink, COLORS.orange]}
-        style={styles.linearGradient}>
-        <StatusBar
-          barStyle={'light-content'}
-          backgroundColor={COLORS.pink}
-          translucent
-        />
-        <View style={[styles.childrenContainer, style]}>{children}</View>
-      </LinearGradient>
-    </SafeAreaView>
-  );
-};
+const BasePage = ({children, style}) => (
+  <SafeAreaView style={styles.safeAreaView}>
+    <LinearGradient
+      start={{x: 1, y: 0}}
+      end={{x: 0, y: 1}}
+      colors={[COLORS.pink, COLORS.orange]}
+      style={styles.linearGradient}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={COLORS.pink}
+        translucent
+      />
+      <View style={[styles.childrenContainer, style]}>{children}</View>
+    </LinearGradient>
+  </SafeAreaView>
+);
 
 BasePage.propTypes = {
   children: PropTypes.node.isRequired,
--- src/screens/findCocktail/styles.js
+++ src/screens/findCocktail/styles.js
@@ -1,13 +1,6 @@
 import {StyleSheet} from 'react-native';
-import COLORS from '../../constants/colors';
 
 const styles = StyleSheet.create({
-  inputContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: COLORS.white,
-  },
   activityIndicator: {
     flex: 1,
     justifyContent: 'center',
